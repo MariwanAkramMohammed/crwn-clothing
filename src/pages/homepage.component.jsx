b9455a146468a04500f9ddabf99009e91dc32ba2
MariwanAkramMohammed <MariwanAkramMohammed@gmail.com>
2022-04-26 02:23:41 +0300

    we break down our home page to smaller component ,then we finished all of those style we wanted to apply

---
+++ src/pages/homepage.component.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Director from "../Components/Directory/Directory.Components";
+import "./HomePage.style.scss";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Director />
+  </div>
+);
+export default HomePage;
